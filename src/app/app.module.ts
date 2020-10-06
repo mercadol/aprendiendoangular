ab8d362e839388c091f789faede32c6fd3095b96
Leonardo Mercado <lamercadob1@gmail.com>
2020-10-06 09:41:28 -0500

    modulos-appmodulests, y componentes Header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFileUploaderModule } from "angular-file-uploader";
 
 import { AppComponent } from './app.component';
+import { MiComponente } from './components/micomponente/micomponente.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginarandComponent } from './components/paginarand/paginarand.component';
+import { ErrorComponent } from './components/error/error.component';
+import { EsParPipe } from './pipes/espar.pipe';
+import { from } from 'rxjs';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { MomentModule } from 'angular2-moment';
+import { ArticleComponent } from './components/article/article.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArticleNewComponent } from './components/article-new/article-new.component';
+import { ArticleEditComponent } from './components/article-edit/article-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente,
+    PeliculasComponent,
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginarandComponent,
+    ErrorComponent,
+    EsParPipe,
+    ArticlesComponent,
+    ArticleComponent,
+    SearchComponent,
+    ArticleNewComponent,
+    ArticleEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpClientModule,
+    MomentModule,
+    AngularFileUploaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
